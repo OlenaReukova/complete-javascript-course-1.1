82721c7f455ea8c8398ee7c5f6e21b1df4480082
Olena Reukova <kronverk01@gmail.com>
2023-04-08 15:05:04 +0100

    Fundamentals – Part 1
    Values and Variables
    Data Types
    let, const and var

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,20 @@
+let country = "UK";
+let continent = "Europe";
+let population = 67.33;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = true;
+let language = "English";
+
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
+
+//let, const and var
+
+language = "french";
+console.log(language);
