d4c5676454857105d0599aa10fba08b785653943
Olena Reukova <kronverk01@gmail.com>
2023-04-29 00:20:14 +0100

    use strict

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,6 @@
+//activation of strick mode
+//Important!!! this line should be first
+`use strict`;
+
+//strict mode help us to avoid bugs
+// we recieve a msg about mistakes in code and shows reserved words we can't use
