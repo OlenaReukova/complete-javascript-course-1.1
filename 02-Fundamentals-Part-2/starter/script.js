185cb1304cc870935f7e734947fec48dfd1a48f5
Olena Reukova <kronverk01@gmail.com>
2023-04-29 10:47:03 +0100

    Fundamental part 2 - Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -4,3 +4,17 @@
 
 //strict mode help us to avoid bugs
 // we recieve a msg about mistakes in code and shows reserved words we can't use
+
+//Functions
+function describeCountry(country, population, capitalCity) {
+  const countryInfo = `${country} has ${population} million people and its capital city is ${capitalCity}`;
+  return countryInfo;
+  //return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+}
+
+const countryInformation = describeCountry("France", 67.7, "Paris");
+console.log(countryInformation);
+const countryInformation2 = describeCountry("UK", 67, "London");
+console.log(countryInformation2);
+const countryInformation3 = describeCountry("USA", 331, "Washington");
+console.log(countryInformation3);
